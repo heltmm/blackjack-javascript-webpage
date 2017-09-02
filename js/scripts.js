34679b9b50267959805937c19a52611554f3c6b9
Mark Helt <heltmm@gmail.com>
2017-09-01 20:44:02 -0700

    creat card object with suit and rank, creat a deck of cards using a double for loop, use a shuffle alorithim to shuffle the array of cards

---
+++ js/scripts.js
@@ -0,0 +1,36 @@
+
+var values = {'A':1, '2':2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9, 'T':10, 'J':10, 'Q':10, 'K':10}
+
+
+//creat a card
+function Card(suit, rank){
+  this.suit = suit;
+  this.rank = rank;
+}
+
+//creat a deck of cards
+function Deck() {
+  this.deck = [];
+  var suits = ['C', 'S', 'H', 'D'];
+  var ranks = ['A', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K'];
+  for(var i = 0; i<suits.length; i++ ){
+    for(var j = 0; j<ranks.length; j++){
+      this.deck.push(new Card(suits[i],ranks[j]))
+    }
+  }
+}
+//Randomize deck using Durstenfield shuffle algorithm
+Deck.prototype.shuffle = function() {
+  for (var i = this.deck.length - 1; i > 0; i--) {
+      var j = Math.floor(Math.random() * (i + 1));
+      var temp = this.deck[i];
+      this.deck[i] = this.deck[j];
+      this.deck[j] = temp;
+  }
+  return this.deck;
+}
+
+var newDeck = new Deck();
+console.log(newDeck)
+newDeck.shuffle()
+console.log(newDeck)
