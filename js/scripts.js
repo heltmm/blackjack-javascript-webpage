d913cd976a1c93fdba9d0be96025db0d955ce684
Mark Helt <heltmm@gmail.com>
2017-09-02 09:49:52 -0700

    add logic to draw hand

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,28 @@
 var suits = ['C', 'S', 'H', 'D'];
 var ranks = ['A', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K'];
 var card_size = [72, 96];
+var x_start = 0
+var y_start = 0
 //card Object
 function Card(suit, rank){
   this.suit = suit;
   this.rank = rank;
 }
+//draws a specific card from cards image of all cards treating it like an array as it selects each card based on index of suits and ranks
 Card.prototype.drawCard = function(card){
-  ctx.drawImage(img, (card_size[0] * ranks.indexOf(this.rank)), (card_size[1] * suits.indexOf(this.suit)) , card_size[0], card_size[1], 50, 50, card_size[0], card_size[1])
-  // ctx.drawImage(img, 0, 0, card_size[0], card_size[1], 50, 50, card_size[0], card_size[1])
-  console.log(ranks[this.rank])
+  ctx.drawImage(img, (card_size[0] * ranks.indexOf(this.rank)), (card_size[1] * suits.indexOf(this.suit)) , card_size[0], card_size[1], x_start, y_start, card_size[0], card_size[1])
 }
-// card_size = [72, 96]
-// context.drawImage(img,sx,sy,swidth,sheight,x,y,width,height);
-
 //hand object
 function Hand(){
   this.hand = [];
   this.value = 0;
   this.hasAce = false;
 }
+//adds a card to the hand
 Hand.prototype.addCard = function(card){
   this.hand.push(card)
 }
+//gets value of hand including logic for aces
 Hand.prototype.getValue = function(){
   var values = {'A':1, '2':2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9, 'T':10, 'J':10, 'Q':10, 'K':10}
   this.value = 0;
@@ -35,6 +35,13 @@ Hand.prototype.getValue = function(){
   }if(this.value <=11 && this.hasAce === true){
     this.value += 10;
   }return this.value;
+}
+Hand.prototype.drawHand = function(){
+  x_start = 0;
+  for(var i = 0; i < this.hand.length; i ++){
+    this.hand[i].drawCard();
+    x_start += 80;
+  }
 }
 
 //Deck object of cards
@@ -56,40 +63,36 @@ Deck.prototype.shuffle = function() {
   }
   return this.deck;
 }
+//removes the top card from the deck and returns it
 Deck.prototype.deal = function(){
   return this.deck.pop(0);
 }
-
+//resets variables and starts a new game
 function newGame(){
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
   deck = new Deck();
   deck.shuffle();
   playerHand = new Hand();
   dealerHand = new Hand();
-  testCard = new Card('D', 'A')
-  testCard.drawCard();
-  console.log(ranks.indexOf('A'))
+
 }
 
 //front end
 $(document).ready(function(){
-  //creat basic gui to display cards
+  //creates basic gui to modify canvas
   canvas = document.getElementById("myCanvas");
   ctx = canvas.getContext("2d");
   img = document.getElementById("cards");
-  ctx.drawImage(img, 0, 0, card_size[0], card_size[1], 50, 50, card_size[0], card_size[1])
-  // ctx.drawImage(img,10,10);
-
 
   $("#newGameButton").click(function(){
     newGame();
-    testCard = new Card('D', 'A')
-    testCard.drawCard();
-    console.log(playerHand.value)
+
   });
   $("#hitButton").click(function(){
     if(playerHand.value < 21){
       playerHand.addCard(deck.deal())
       playerHand.getValue();
+      playerHand.drawHand();
       console.log(playerHand)
       console.log(playerHand.value)
     }
