0a1f1a243c23ad0b789f8b954e902c980d1f49b1
Mark Helt <heltmm@gmail.com>
2017-09-01 21:00:03 -0700

    creat hand object and prototype to add card to hand

--- js/scripts.js
+++ js/scripts.js
@@ -2,13 +2,20 @@
 var values = {'A':1, '2':2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9, 'T':10, 'J':10, 'Q':10, 'K':10}
 
 
-//creat a card
+//card Object
 function Card(suit, rank){
   this.suit = suit;
   this.rank = rank;
 }
+//hand object
+function Hand(){
+  this.hand = [];
+}
+Hand.prototype.addCard = function(card){
+  this.hand.push(card)
+}
 
-//creat a deck of cards
+//deck object
 function Deck() {
   this.deck = [];
   var suits = ['C', 'S', 'H', 'D'];
